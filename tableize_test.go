a782d42784c8b87b5a97e90246bf9d68f6717aaf
Dominic Barnes <dominic@dbarnes.info>
2018-03-13 14:09:49 -0600

    feat: iterate maps deterministically

--- tableize_test.go
+++ tableize_test.go
@@ -1,61 +1,111 @@
-package tableize
+package tableize_test
 
-import "github.com/bmizerany/assert"
-import "encoding/json"
-import "testing"
+import (
+	"encoding/json"
+	"testing"
 
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+	. "github.com/segmentio/go-tableize"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestTableize(t *testing.T) {
-	event := map[string]interface{}{
-		"name": map[string]interface{}{
-			"first   name  ": "tobi",
-			"Last-Name":      "holowaychuk",
-			"NickName":       "shupa",
-			"$some_thing":    "tobi",
+	spec := []struct {
+		desc     string
+		input    Input
+		expected map[string]interface{}
+	}{
+		{
+			desc: "simple",
+			input: Input{Value: map[string]interface{}{
+				"species": "ferret",
+			}},
+			expected: map[string]interface{}{
+				"species": "ferret",
+			},
 		},
-		"species": "ferret",
+		{
+			desc: "nested",
+			input: Input{
+				Value: map[string]interface{}{
+					"name": map[string]interface{}{
+						"first": "tobi",
+					},
+				},
+			},
+			expected: map[string]interface{}{"name_first": "tobi"},
+		},
+		{
+			desc: "normalize keys",
+			input: Input{
+				Value: map[string]interface{}{
+					"first   name  ": "tobi",
+					"Last-Name":      "holowaychuk",
+					"NickName":       "shupa",
+					"$some_thing":    "tobi",
+				},
+			},
+			expected: map[string]interface{}{
+				"first_name": "tobi",
+				"last_name":  "holowaychuk",
+				"nick_name":  "shupa",
+				"some_thing": "tobi",
+			},
+		},
+		{
+			desc: "conflicting keys",
+			input: Input{
+				Value: map[string]interface{}{
+					"firstName":   "first_1",
+					"first_name":  "first_2",
+					"lastName":    "last_1",
+					"last_name":   "last_2",
+					"middleName":  "middle_1",
+					"middle_name": "middle_2",
+					"first":       map[string]interface{}{"name": "first_3"},
+					"last":        map[string]interface{}{"name": "last_3"},
+				},
+			},
+			expected: map[string]interface{}{
+				"first_name":  "first_2",
+				"last_name":   "last_2",
+				"middle_name": "middle_2",
+			},
+		},
+		{
+			desc: "substitutions",
+			input: Input{
+				Value: map[string]interface{}{
+					"name": map[string]interface{}{
+						"first   name  ": "tobi",
+						"Last-Name":      "holowaychuk",
+						"NickName":       "shupa",
+						"$some_thing":    "tobi",
+					},
+					"_mid":    "value",
+					"species": "ferret",
+				},
+				Substitutions: map[string]string{
+					"species":          "r_species",
+					"name_$some_thing": "just_some_thing",
+					"_mid":             "u_mid",
+				},
+			},
+			expected: map[string]interface{}{
+				"name_first_name":      "tobi",
+				"name_last_name":       "holowaychuk",
+				"r_species":            "ferret",
+				"name_nick_name":       "shupa",
+				"name_just_some_thing": "tobi",
+				"u_mid":                "value",
+			},
+		},
 	}
 
-	flat := Tableize(&Input{Value: event})
-	assert.Equal(t, flat["name_first_name"], "tobi")
-	assert.Equal(t, flat["name_last_name"], "holowaychuk")
-	assert.Equal(t, flat["species"], "ferret")
-	assert.Equal(t, flat["name_nick_name"], "shupa")
-	assert.Equal(t, flat["name_some_thing"], "tobi")
-}
-
-func TestTableizeWithSubstitution(t *testing.T) {
-	event := map[string]interface{}{
-		"name": map[string]interface{}{
-			"first   name  ": "tobi",
-			"Last-Name":      "holowaychuk",
-			"NickName":       "shupa",
-			"$some_thing":    "tobi",
-		},
-		"_mid":    "value",
-		"species": "ferret",
+	for _, test := range spec {
+		t.Run(test.desc, func(t *testing.T) {
+			assert.Equal(t, test.expected, Tableize(&test.input))
+		})
 	}
-
-	flat := Tableize(&Input{
-		Value: event,
-		Substitutions: map[string]string{
-			"species":          "r_species",
-			"name_$some_thing": "just_some_thing",
-			"_mid":             "u_mid",
-		},
-	})
-
-	assert.Equal(t, flat["name_first_name"], "tobi")
-	assert.Equal(t, flat["name_last_name"], "holowaychuk")
-	assert.Equal(t, flat["r_species"], "ferret")
-	assert.Equal(t, flat["name_nick_name"], "shupa")
-	assert.Equal(t, flat["name_just_some_thing"], "tobi")
-	assert.Equal(t, flat["u_mid"], "value")
 }
 
 func BenchmarkSmall(b *testing.B) {
@@ -202,5 +252,11 @@ func BenchmarkLarge(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		Tableize(&Input{Value: event})
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
